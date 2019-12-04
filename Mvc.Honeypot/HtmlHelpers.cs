0b2e78ddf16a81710b547e09b84698181c648202
Travis Pessetto <travis@pessetto.com>
2019-12-04 16:22:37 -0700

    Extend honeypot field to allow attributes

--- Mvc.Honeypot/HtmlHelpers.cs
+++ Mvc.Honeypot/HtmlHelpers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -49,7 +50,7 @@ namespace Mvc.Honeypot
             return sbHashedName.ToString();
         }
 
-        #endregion 
+        #endregion
 
         /// <summary>
         /// Renders out field with honeypot security check enabled
@@ -59,7 +60,23 @@ namespace Mvc.Honeypot
         /// <param name="value">Value of the field</param>
         /// <param name="css">CSS class to be applied to input field</param>
         /// <returns>Returns render out MvcHtmlString for displaying on the View</returns>
-        public static MvcHtmlString HoneyPotField(this HtmlHelper helper, string name, object value, string inputCss = null, InputType fieldType=InputType.Text,string honeypotCss = null, InputType honeypotType=InputType.Hidden)
+        public static MvcHtmlString HoneyPotField(this HtmlHelper helper, string name, object value, string inputCss = null, InputType fieldType = InputType.Text, string honeypotCss = null, InputType honeypotType = InputType.Hidden)
+        {
+            return HoneyPotField(helper, name, value, new { @class = inputCss }, fieldType, honeypotCss, honeypotType);
+        }
+
+        /// <summary>
+        /// Renders out field with honeypot security check enabled
+        /// </summary>
+        /// <param name="helper">HtmlHelper which will be extended</param>
+        /// <param name="name">Name of field. Should match model field of string type</param>
+        /// <param name="value">Value of the field</param>
+        /// <param name="htmlAttributes">Attributes to add to the HTML tag</param>
+        /// <param name="fieldType">The field type</param>
+        /// <param name="honeypotCss">The CSS class applied to the honeypot HTML tag</param>
+        /// <param name="honeypotType">The type of input of the honeypot</param>
+        /// <returns>Returns render out MvcHtmlString for displaying on the View</returns>
+        public static MvcHtmlString HoneyPotField(this HtmlHelper helper, string name, object value, dynamic htmlAttributes = null, InputType fieldType=InputType.Text,string honeypotCss = null, InputType honeypotType=InputType.Hidden)
         {
             StringBuilder sbControlHtml = new StringBuilder();
             using (StringWriter stringWriter = new StringWriter())
@@ -71,9 +88,14 @@ namespace Mvc.Honeypot
                     hashedField.Value = value != null ? value.ToString() : string.Empty;
                     hashedField.ID = hashedName;
                     hashedField.Name = hashedName;
-                    if (!string.IsNullOrWhiteSpace(inputCss))
+                    if (htmlAttributes != null)
                     {
-                        hashedField.Attributes["class"] = inputCss;
+                        foreach (var property in htmlAttributes.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                        {
+                            var attributeName = property.Name.Replace('_','-');
+                            var attributeValue = property.GetValue(htmlAttributes, null);
+                            hashedField.Attributes[attributeName] = attributeValue;
+                        }
                     }
                     hashedField.RenderControl(htmlWriter);
 
@@ -92,8 +114,13 @@ namespace Mvc.Honeypot
             }
             return new MvcHtmlString(sbControlHtml.ToString());
         }
-        
+
         public static MvcHtmlString HoneyPotTextArea(this HtmlHelper helper, string name, object value, int cols, int rows, string inputCss = null, string honeypotCss = null)
+        {
+            return HoneyPotTextArea(helper, name, value, cols, rows, new { @class = inputCss }, honeypotCss);
+        }
+
+        public static MvcHtmlString HoneyPotTextArea(this HtmlHelper helper, string name, object value, int cols, int rows, dynamic htmlAttributes, string honeypotCss = null)
         {
             StringBuilder sbControlHtml = new StringBuilder();
             using (StringWriter stringWriter = new StringWriter())
@@ -107,9 +134,14 @@ namespace Mvc.Honeypot
                     hashedField.Name = hashedName;
                     hashedField.Cols = cols;
                     hashedField.Rows = rows;
-                    if (!string.IsNullOrWhiteSpace(inputCss))
+                    if (htmlAttributes != null)
                     {
-                        hashedField.Attributes["class"] = inputCss;
+                        foreach (var property in htmlAttributes.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                        {
+                            var attributeName = property.Name.Replace('_', '-');
+                            var attributeValue = property.GetValue(htmlAttributes, null);
+                            hashedField.Attributes[attributeName] = attributeValue;
+                        }
                     }
                     hashedField.RenderControl(htmlWriter);
 
